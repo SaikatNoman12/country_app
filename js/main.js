e2c559bd363747cd6123312a8b5f7976cbf17ed5
saikatnoman12 <noommaa240@gmail.com>
2022-11-02 23:43:53 +0600

    complete get api data in searching

--- js/main.js
+++ js/main.js
@@ -1,31 +1,74 @@
 const form = document.forms[0];
-const input = document.querySelector('.search-input');
+const inputData = document.querySelector('.search-input');
+const details = document.querySelector('.details');
 
 
-form.addEventListener('click', (e) => {
+form.addEventListener('submit', (e) => {
 
     e.preventDefault();
 
-    // const inputValue = input.value;
-    const inputValue = 'india';
+    const inputValue = inputData.value.toLowerCase();
 
-    const url = `https://restcountries.com/v3.1/name/${inputValue}?fullText=true`;
+    if (inputValue === '' || inputValue.trim() === '') {
+        console.log('please fill your input!');
+    }
+    else {
 
-    fetch(url)
-        .then(res => res.json())
-        .then(data => showData(data[0]))
-        .catch(() => {
-            console.log('not found');
-        })
+        const countryName = `https://restcountries.com/v3.1/name/${inputValue}?fullText=true`;
+
+        fetch(countryName)
+            .then(res => res.json())
+            .then(data => showData(data[0]))
+            .catch(() => {
+
+            })
+
+
+    }
 
 });
 
 
 function showData(data) {
-    console.log(data);
-    console.log(data.capital[0]);
-    console.log(data.population);
-    console.log(data.flags.svg);
-    console.log(data.independent ? 'Independent' : 'Unindependent');
-    
+
+    details.innerHTML =
+        `<div class="flag-country-name">
+        <div class="flag">
+            <img src="${data.flags.svg}" alt="${data.name.common.toLowerCase()}">
+        </div>
+        <div class="country-name">
+            <h1>${data.name.common}</h1>
+        </div>
+        </div>
+
+        <div class="info">
+
+            <div class="column">
+                <h2>Capital:</h2>
+                <span>${data.continents[0]}</span>
+            </div>
+
+            <div class="column">
+                <h2>Population:</h2>
+                <span>${data.population}</span>
+            </div>
+
+            <div class="column">
+                <h2>Currencies:</h2>
+                <span>${data.currencies[Object.keys(data.currencies)].name}</span>
+            </div>
+
+
+            <div class="column">
+                <h2>Languages:</h2>
+                <span>${Object.values(data.languages).toString().split(',').join(', ')}</span>
+            </div>
+
+            <div class="column">
+                <h2>Self-reliant::</h2>
+                <span>${data.independent ? 'Independent' : 'Unindependent'}</span>
+            </div>
+
+        </div>`;
+
 }
